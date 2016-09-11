cfdbcce1ebd2d0f459576fca5f566e4900b19cca
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-09-11 21:17:26 +0900

    notification window appear by clicking the notification button

--- src/app/header/notification-header/notification-header.component.ts
+++ src/app/header/notification-header/notification-header.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-notification-header',
   templateUrl: './notification-header.component.html',
-  styleUrls: ['./notification-header.component.scss']
+  styleUrls: ['./notification-header.component.scss'],
+  host: {'(document:click)': 'onClick($event)'}
 })
 export class NotificationHeaderComponent implements OnInit {
 
   notification_arr = ["aa"];
+  is_open = false;
 
-  constructor() { }
+  constructor(private _eref: ElementRef) { }
 
   ngOnInit() {
+  }
+
+  toggle_window(){
+    this.is_open = !this.is_open
+  }
+
+// http://stackoverflow.com/questions/35712379/angular2-close-dropdown-on-click-outside-is-there-an-easiest-way
+// http://blog.yuhiisk.com/archive/2016/05/01/angular2-dom-manupilation.html
+
+
+  onClick(event){
+    if (!this._eref.nativeElement.contains(event.target)){
+      this.is_open = false;
+    }
+
   }
 
 }
