abdccec279e2826e9eb7caaf322f457ec29f7465
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-09-13 22:23:52 +0900

    user info is registered with security

--- src/app/shared/userauth.service.ts
+++ src/app/shared/userauth.service.ts
@@ -1,14 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AngularFire } from 'angularfire2';
+import { AngularFire , FirebaseObjectObservable} from 'angularfire2';
 import {User, User_Search} from './../interface/user'
 import {BehaviorSubject } from 'rxjs/Rx'
 
 @Injectable()
 export class UserauthService {
 
-  constructor(public af: AngularFire) { }
+  constructor(public af: AngularFire) {
 
-  id: string;
+/* this code is still not working, but it will come soon.*/ 
+    af.auth.subscribe((auth) => {
+      console.log("auth status changed");
+      console.log(auth);
+    })
+///////////////////////
+
+
+   }
+
+
+  own_user_id: string;
   own_user: User = { loggedIn:false,full_name:"",short_name:"",pict_src:""};
   own_user_subject$ = new BehaviorSubject(this.own_user);
 
@@ -26,7 +37,7 @@ export class UserauthService {
       const full_name = result.auth.displayName;
       const split_name_arr = full_name.split(" ");
 
-      this.id = result.uid;
+      this.own_user_id = result.uid;
 
       this.own_user ={
           loggedIn : true,
@@ -35,40 +46,39 @@ export class UserauthService {
           pict_src : result.auth.photoURL
       }
       this.own_user_subject$.next(this.own_user);
-      console.log(this.own_user);
+      this.register_user();
 
       const name1 = split_name_arr[0];
       const name2 = split_name_arr[1];
       const name3 = split_name_arr[2];
       const own_searach_user : User_Search = { name1, name2, name3};
       console.log(own_searach_user);
-    //    this.own_user_subject.next(own_user);
-      //  this.register_user(own_user);
     });
-
-
-/*
-    this.user.loggedIn = true;
-    this.user.profile_pict = "https://scontent.xx.fbcdn.net/v/t1.0-1/p100x100/12341602_953593204721655_7227562974230394333_n.jpg?oh=5f62d11e552397a29b3d1343825fc149&oe=58451A03";
-    this.user.first_name = "yuta";
-    this.user.last_name = "moriyama";
-    */
   }
+
+
 
   logout = function(){
     this.user.loggedIn = false;
   }
 
 
-  private register_user = function(user_oj : User){
-    /*
-    let user_obj_db = {
-      name : user_oj.name,
-      pict_src :user_oj.pict_src
+  private register_user = ()　=>{
+
+    if(!this.own_user_id || !this.own_user.loggedIn ){
+      alert("cannot register user");
+      return;
     }
-    let user_firebase_ref = this.af.database.object('/users/' + user_oj.id);
-    user_firebase_ref.set(user_obj_db);
-    */
+
+    const own_data_regist : User = {
+      full_name: this.own_user.full_name,
+      short_name: this.own_user.short_name,
+      pict_src: this.own_user.pict_src
+    }
+
+    const userObservable = this.af.database.object('/users/user_basic/' + this.own_user_id);
+    userObservable.update(own_data_regist);
+
   }
 
 
