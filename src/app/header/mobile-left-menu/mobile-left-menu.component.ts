798a83cadb2ab209bd9d6293018019c4cb14a35c
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-09-18 03:40:52 +0900

    animation from void to * and use translateX

--- src/app/header/mobile-left-menu/mobile-left-menu.component.ts
+++ src/app/header/mobile-left-menu/mobile-left-menu.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit, Input,
   selector: 'app-mobile-left-menu',
   templateUrl: './mobile-left-menu.component.html',
   styleUrls: ['./mobile-left-menu.component.scss'],
+  /*
   animations: [
     trigger("test_animate_state",[
       state("active", style({width:"0px", backgroundColor:"#ff0000"})),
@@ -12,23 +13,26 @@ import { Component, OnInit, Input,
       transition("inactive=>active", animate("500ms ease-in")),
       transition("active=>inactive", animate("500ms ease-out"))
     ])
-
   ]  
+  */
+  animations: [
+    trigger("flyInOut",[
+      state("in", style({transform:"translateX(0)"})),
+      transition("void=>*", [style({transform: 'translateX(-200px)'}), animate(200)]),
+      /*,transition("*=>void", [style({transform: 'translateX(0)'}), animate(200)])*/
+      transition("*=>void", [animate(1000, style({transform: 'translateX(100%)'}))])
+      
+    ])
+  ]
 })
 
 export class MobileLeftMenuComponent implements OnInit {
-
-
   show_mobile_menu = false;
   is_open = "inactive";
-
   constructor() { }
-
   ngOnInit() {
   }
-
-
-
+/*
   @Input()
   set mobile_menu_oepn(mobile_menu_oepn: boolean) {
     this.show_mobile_menu = mobile_menu_oepn;
@@ -38,9 +42,7 @@ export class MobileLeftMenuComponent implements OnInit {
       this.is_open = "inactive";
     }
     console.log(this.is_open );
-
   }
-
-
+*/
 
 }
