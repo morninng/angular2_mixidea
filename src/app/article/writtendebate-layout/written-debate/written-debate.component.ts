d99a2c3d417a79bbad6b338f1efa477ebea9b278
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-10-02 16:43:14 +0900

    to reflesh

--- src/app/article/writtendebate-layout/written-debate/written-debate.component.ts
+++ src/app/article/writtendebate-layout/written-debate/written-debate.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params  }     from '@angular/router';
 import {AngularFire, FirebaseObjectObservable} from 'angularfire2';
+import { UserauthService} from './../../../shared/userauth.service';
+
+
 
 @Component({
   selector: 'app-written-debate',
@@ -11,10 +14,9 @@ export class WrittenDebateComponent implements OnInit {
 
   written_debate_data: FirebaseObjectObservable<any>;
   event_data: FirebaseObjectObservable<any>;
-  constructor(private route: ActivatedRoute, private af: AngularFire) { }
+  constructor(private route: ActivatedRoute, private af: AngularFire, private user_auth : UserauthService) { }
 
   ngOnInit() {
-    console.log("written debate component initialize");
 
     this.route.params.forEach((params: Params) => {
       let event_id = params['id']; 
@@ -22,7 +24,15 @@ export class WrittenDebateComponent implements OnInit {
 
       this.written_debate_data = this.af.database.object('/event_related/written_debate/' + event_id);
       this.event_data = this.af.database.object('/event_related/event/' + event_id);
+      console.log("event data");
     });
+
+    if(!this.user_auth.own_user.loggedIn){
+      alert("you need to login to see event data");
+      this.user_auth.open_login_modal();
+    }
+    
+
   }
  
 }
