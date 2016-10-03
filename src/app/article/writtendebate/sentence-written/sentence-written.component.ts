6213af8e35c546cd7eb00bdfe76f672c44d3cc2e
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-10-04 02:19:03 +0900

    balloon is shown for to open sentence comment

--- src/app/article/writtendebate/sentence-written/sentence-written.component.ts
+++ src/app/article/writtendebate/sentence-written/sentence-written.component.ts
@@ -7,11 +7,24 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class SentenceWrittenComponent implements OnInit {
 
+  show_balloon : boolean;
   @Input() content;
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  content_mouseenter(){
+    this.show_balloon = true;
+  }
+  content_mouseleave(){
+    this.show_balloon = false;
+  }
+
+  open_sentence_comment(){
+    console.log("open_sentence_comment");
   }
 
 }
--- src/app/article/writtendebate/transcription/transcription.component.ts
+++ src/app/article/writtendebate/transcription/transcription.component.ts
@@ -15,4 +15,8 @@ export class TranscriptionComponent implements OnInit {
   ngOnInit() {
   }
 
+  
+
+
+
 }
