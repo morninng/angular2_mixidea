e0ba5380c2a78aadc265f65c2f15fdb2c6ebdb40
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-10-03 19:51:39 +0900

    opinion component has been created

--- src/app/article/writtendebate/opinion/opinion.component.ts
+++ src/app/article/writtendebate/opinion/opinion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-opinion',
@@ -8,16 +8,40 @@ import { Component, OnInit, Input } from '@angular/core';
 export class OpinionComponent implements OnInit {
 
   @Input() opinion_id :string;
-  @Input() opinion :string;
+  @Input() opinion :any;
   @Input() subsequent_id :string;
   @Input() opinion_status : string;
 
-  constructor() { }
+  _el;
+  audio_element;
 
+  constructor(private el: ElementRef) { }
+
+
+  ngAfterViewInit(){
+
+    this._el = this.el.nativeElement;
+    const audio_container = this._el.getElementsByClassName("audio_container")[0];
+
+    if(this.opinion.audio_url){
+
+      console.log(this.opinion.audio_url);
+      this.audio_element = new Audio();
+      this.audio_element.controls = true;
+      this.audio_element.src = this.opinion.audio_url;
+      this.audio_element.addEventListener("play", ()=>{ console.log("play"); });
+      this.audio_element.addEventListener("seeked", ()=>{ console.log("seeked"); });
+      this.audio_element.addEventListener("timeupdate", ()=>{ console.log("time update"); });
+      audio_container.insertBefore(this.audio_element, null);
+
+    }
+  }
 
 
   ngOnInit() {
     
   }
+
+
 
 }
