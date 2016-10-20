47cd9ea944a9049fd6fff6bdf4c4f838e6edcec0
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-10-21 02:05:08 +0900

    change file name

--- src/app/event/eventlist/eachlist-online-debate-written/eachlist-online-debate-written.component.spec.ts
+++ src/app/event/eventlist/eachlist-online-debate-written/eachlist-online-debate-written.component.spec.ts
@@ -1,11 +1,11 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { EachlistOnlineDebateWrittenComponent } from './each-eventlist.component';
+import { EachEventlistComponent } from './each-eventlist.component';
 
 describe('Component: EachlistOnlineDebateWritten', () => {
   it('should create an instance', () => {
-    let component = new EachlistOnlineDebateWrittenComponent();
+    let component = new EachEventlistComponent();
     expect(component).toBeTruthy();
   });
 });
